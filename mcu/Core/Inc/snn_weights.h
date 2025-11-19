99d5cfed4ac43f3072f8124b36a9f7b72cf9b9dc
Piorosen <chacha@udon.party>
2025-11-20 07:15:02 +0900

    haha

--- mcu/Core/Inc/snn_weights.h
+++ mcu/Core/Inc/snn_weights.h
@@ -1,29 +1,28 @@
-/* auto-generated SNN weights (declaration) */
-
-#ifndef SNN_WEIGHTS_H
-#define SNN_WEIGHTS_H
+/* Auto-generated quantized SNN weights (declaration) */
+#ifndef CHACHA_SNN_WEIGHTS_H
+#define CHACHA_SNN_WEIGHTS_H
 
 #include <stdint.h>
 
-/* Auto-generated SNN weight declarations (int8 quantized) */
-/* Quantization: per-tensor symmetric, float ~= int8 * scale */
-
+// Conv1
 extern const int8_t snn_conv1_weight[32][3][5][5];
 extern const float snn_conv1_weight_scale;
 extern const int8_t snn_conv1_bias[32];
 extern const float snn_conv1_bias_scale;
 
+// Conv2
 extern const int8_t snn_conv2_weight[64][32][5][5];
 extern const float snn_conv2_weight_scale;
 extern const int8_t snn_conv2_bias[64];
 extern const float snn_conv2_bias_scale;
 
+// FC
 extern const int8_t snn_fc_weight[10][4096];
 extern const float snn_fc_weight_scale;
 extern const int8_t snn_fc_bias[10];
 extern const float snn_fc_bias_scale;
 
-// LIF parameters (not quantized)
+// LIF parameters (float)
 extern const float snn_lif1_beta;
 extern const float snn_lif2_beta;
 extern const float snn_lif_out_beta;
@@ -31,4 +30,4 @@ extern const float snn_lif1_threshold;
 extern const float snn_lif2_threshold;
 extern const float snn_lif_out_threshold;
 
-#endif /* SNN_WEIGHTS_H */
+#endif /* CHACHA_SNN_WEIGHTS_H */
